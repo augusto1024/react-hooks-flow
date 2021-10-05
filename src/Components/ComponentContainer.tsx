5ef9c351bec53ec1579997de61ee79496f39f824
Augusto Alonso <53446634+augusto1024@users.noreply.github.com>
2021-10-05 09:18:34 -0300

    Add Tailwind

---
+++ src/Components/ComponentContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComponentContainer = (): JSX.Element => {
+  return (
+    <div className="bg-white shadow-md m-2 rounded-md w-full p-5">
+      Tailwind Test
+    </div>
+  );
+};
+
+export default ComponentContainer;
